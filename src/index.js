0c32d96d4fe304c6ea15fd442790300aafb613c1
Soubhagya pradhan <soubhagya@APPLEs-MacBook-Pro.local>
2022-09-21 19:51:02 +0530

    added auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <Auth0Provider
+      domain="localhost:3000"
+      clientId="MToy0ApInf5dDYIwifPsz"
+      redirectUri={window.location.origin}
+    >
+     <App />
+    </Auth0Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
