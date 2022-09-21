5ba9e1172dbfd1822ad1691dfd94e91e773315b6
Soubhagya pradhan <soubhagya@APPLEs-MacBook-Pro.local>
2022-09-21 21:05:14 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ const LoginButton = () => {
           <h2>{user.name}</h2>
           <p>{user.email}</p>
 
+          <div>
+            {
+              JSON.stringify(user)
+            }
+          </div>
+
           <button onClick={() => logout({ returnTo: window.location.origin })}>
             Log Out
           </button>
