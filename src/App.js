9e127f225a1df2e945b876e946bf9c813ab628d0
Soubhagya pradhan <soubhagya@APPLEs-MacBook-Pro.local>
2022-09-21 20:54:04 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated, isLoading, logout } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div>
+      {isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+          </button>
+        </div>
+      )}
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </div>
+  );
 };
 
 export default LoginButton;
